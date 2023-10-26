693046d05abfd8e238820ca58b180fdbdd392b4a
andi9g <andirizky.bayuputra@gmail.com>
2023-10-26 10:44:56 +0700

    oke

--- resources/views/laporan/raport/nilai.blade.php
+++ resources/views/laporan/raport/nilai.blade.php
@@ -225,7 +225,7 @@
                     <td rowspan="{{ $nilai }}">{{ $item["namamapel"] }}</td>
                     <td align="center" rowspan="{{ $nilai }}">{{ $item["nilai"] }}</td>
                     
-                    @if ($item["agama"] == true && $item['ketAgama'] != "Islam") 
+                    @if ($item['ketAgama'] != "Islam") 
                         <td style="page-break-before: always;" class="fontku2">{{ empty($item["catatanAgama"])?'-':$item["catatanAgama"] }}</td>
                     @else
                     @if ($ket=="capaian")
@@ -239,7 +239,7 @@
                     @endif
                     
                 </tr>
-                @if ($item["agama"] == false && $item['ketAgama'] == "Islam") 
+                @if ($item['ketAgama'] == "Islam") 
                 @if ($ket=="keduanya")
                 <tr>
                     <td style="page-break-before: always;" class="fontku2">{{ $item["catatan"] }}</td>
@@ -269,7 +269,7 @@
                         $ket = "keduanya";
                         $nilai = 2;
                     }
-                    if($item["agama"]==true && $item['ketAgama'] != "Islam") {
+                    if($item['ketAgama'] != "Islam") {
                         $nilai = 1;
                         // dd($item["catatanAgama"].'error');
                     }
@@ -279,7 +279,7 @@
                     <td rowspan="{{ $nilai }}" align="center">{{ $loop->iteration }}</td>
                     <td rowspan="{{ $nilai }}">{{ $item["namamapel"] }}</td>
                     <td align="center" rowspan="{{ $nilai }}">{{ $item["nilai"] }}</td>
-                    @if ($item["agama"] == true && $item['ketAgama'] != "Islam") 
+                    @if ($item['ketAgama'] != "Islam") 
                         <td style="page-break-before: always;" class="fontku2">{{ empty($item["catatanAgama"])?'-':$item["catatanAgama"] }}</td>
                     @else
                     @if ($ket=="capaian")
@@ -293,7 +293,7 @@
                     @endif
 
                 </tr>
-                @if ($item["agama"] == false && $item['ketAgama'] == "Islam")
+                @if ($item['ketAgama'] == "Islam")
                     @if ($ket=="keduanya")
                     <tr>
                         <td style="page-break-before: always;" class="fontku2">{{ $item["catatan"] }}</td>
