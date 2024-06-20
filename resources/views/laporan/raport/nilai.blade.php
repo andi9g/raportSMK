8f778e7f52a8c3445cd44375466b836a42fefcb4
andi9g <andirizky.bayuputra@gmail.com>
2024-06-20 18:42:40 +0700

    oke

--- resources/views/laporan/raport/nilai.blade.php
+++ resources/views/laporan/raport/nilai.blade.php
@@ -284,21 +284,21 @@
                     }
                 @endphp
                 @if ($item["ket"]=="kejuruan")
-                <tr style="page-break-before: always;">
+                <tr style="page-break-inside: avoid;">
                     <td rowspan="{{ $nilai }}" align="center">{{ $loop->iteration }}</td>
                     <td rowspan="{{ $nilai }}">{{ $item["namamapel"] }}</td>
                     <td align="center" rowspan="{{ $nilai }}">{{ $item["nilai"] }}</td>
                     @if ($item["agama"]==true && $item['ketAgama'] != "Islam")
-                        <td style="page-break-before: always;" class="fontku2">{{ empty($item["catatanAgama"])?'-':$item["catatanAgama"] }}</td>
+                        <td style="page-break-inside: avoid;" class="fontku2">{{ empty($item["catatanAgama"])?'-':$item["catatanAgama"] }}</td>
                     @else
                     @if ($ket=="capaian")
-                        <td style="page-break-before: always;" class="fontku2">
+                        <td style="page-break-inside: avoid;" class="fontku2">
                             {{ $item["capaian"] }}
                         </td>
                     @elseif($ket=="catatan")
-                        <td style="page-break-before: always;" class="fontku2">{{ $item["catatan"] }}</td>
+                        <td style="page-break-inside: avoid;" class="fontku2">{{ $item["catatan"] }}</td>
                     @elseif($ket=="keduanya")
-                        <td style="page-break-before: always;" class="fontku2">{{ $item["capaian"] }}</td>
+                        <td style="page-break-inside: avoid;" class="fontku2">{{ $item["capaian"] }}</td>
                     @endif
 
                     @endif
